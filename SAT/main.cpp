c419ac45fe0fb4853f0a6b0a339a4fa6981ac2e3
Mark Goncharov <goncharov.ma@phystech.edu>
2023-10-07 13:53:40 +0300

    init

---
+++ SAT/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <concepts>
+#include <functional>
+
+using sat_t = std::vector<std::vector<int>>;
+
+template <typename T>
+concept ConvertibleToString = requires(T A) { std::to_string(A); };
+
+std::string variable(int num) {
+    std::string out = num < 0 ? "~" : "";
+    return out + "x" + std::to_string(std::abs(num));
+}
+
+template <typename Iter>
+std::string join(Iter Begin, Iter End, const std::string &Separator, std::function<std::string(typename Iter::value_type)> func = [](typename Iter::value_type Elem) {return Elem;})
+{
+  std::string Result;
+  if (Begin != End) {
+    Result += func(*Begin);
+    ++Begin;
+  }
+  while (Begin != End) {
+    Result += Separator + func(*Begin);
+    ++Begin;
+  }
+  return Result;
+}
+
+void dump_sat(const sat_t &sat, std::string extra_msg = "Current SAT:") {
+    std::cout << extra_msg << "\n";
+    std::vector<std::string> Units;
+    Units.reserve(sat.size());
+
+    for (const auto & conj : sat)
+        Units.push_back("( " + join(conj.begin(), conj.end(), " | ", variable) + " )");
+    
+    std::cout << join(Units.begin(), Units.end(), " & ");
+}
+
+int main() {
+    sat_t sat = {
+        {1, 2, -3},
+        {-1, 2}
+    };
+    dump_sat(sat);
+}
