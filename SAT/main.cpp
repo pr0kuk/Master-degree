6778f4ab7b281f60de41ee0c10d558c97a9cab1f
pr0kuk <alex-shch@yandex.ru>
2023-10-21 19:31:10 +0300

    fixed namespace

--- SAT/main.cpp
+++ SAT/main.cpp
@@ -3,7 +3,7 @@ import Sat;
 import <iostream>;
 
 int main() {
-  Sat_t Sat({{1, 2, -3}, {-1, 2}});
+  Sat::Sat_t Sat({{1, 2, -3}, {-1, 2}});
   Sat.dump("Basic SAT");
   auto Output = Sat.setVar(-1);
   Output.dump("Set SAT -1");
