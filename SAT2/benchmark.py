291b1ad1e8fb3896a5744bcd8880cfb4f5a8a69b
pr0kuk <alex-shch@yandex.ru>
2023-10-25 01:05:45 +0300

    added empty string by default

--- SAT2/benchmark.py
+++ SAT2/benchmark.py
@@ -2,6 +2,8 @@ import sys
 import os
 import subprocess
 flags = {'-dir': [False, []], '-o': [False, []], '-file': [False, []]}
+
+# parsing arguments
 for a in sys.argv[1:]:
     for arg, [flag, lst] in flags.items():
         if a[0] == '-':
@@ -10,14 +12,19 @@ for a in sys.argv[1:]:
             flags[arg][1].append(a)
 dir_path = os.path.dirname(os.path.realpath(__file__)) 
 
-#for o in flags['-o'][1]:
-if flags['-o'][1].len() !=0:
-    o = flags['-o'][1][0]
-    with open(o) as out:
-        for d in flags['-dir'][1]:
-        dir_list = os.listdir(d)
-        for f in dir_list:
-        proc = subprocess.call([os.path.join(dir_path,'build', 'bench'), os.path.join(dir_path,d,f), os.path.join(dir_path,o)])
-        s = o.readlines().split()
-        mean = s[s.index('Mean:')+1]
-        print(mean)
+for d, i in flags['-dir'][1], range(len(flags['-dir'][1])):
+    dir_list = os.listdir(d)
+    o = '' if i >= len(flags['-o'][1]) else flags['-o'][1][i]
+
+    # calling bench.cxx
+    for f in dir_list:
+        proc = subprocess.call([os.path.join(dir_path,'build', 'bench'), os.path.join(dir_path,d,f), os.path.join(dir_path,o) if o!='' else o])
+
+    # reading results
+    if o != '':
+        with open(o) as out:
+            s = out.readlines().split()
+            mean = s[s.index('Mean:')+1]
+            print(mean)
+
+    #plotting graphs
